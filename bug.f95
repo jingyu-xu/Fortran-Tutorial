8fd69e102019e6e29817f71d8874731b2e9c2e67
Jingyu Xu <Jingyu Xu>
2017-03-13 22:55:41 -0400

    show various kinds of errors

---
+++ bug.f95
@@ -0,0 +1,12 @@
+program bug
+
+! this program is full of errors
+real :: a,b,c
+print *, 'enter the value of b:'
+read *, b
+print *, 'enter the value of c'
+read *, c
+
+a = b + c
+print *, a
+end program bug
