133c96a24ddf71d8550eb130ea409c65b8e65f8b
Jingyu Xu <Jingyu Xu>
2017-03-13 23:09:01 -0400

    The do loop

---
+++ loop.f95
@@ -0,0 +1,11 @@
+program loop
+implicit none
+integer :: i
+do i = 0, 20
+	print *, i
+end do
+end program loop
+
+!change the do statement in program loop as follows:
+! do i = 50, 70, 2
+! do i = 5, -5, -2
